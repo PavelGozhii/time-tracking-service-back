3f67f16feb8fcc51c56136dbd70683abf656c8d3
PavelGozhii <pavelgozhii@gmail.com>
2020-04-01 19:56:25 +0300

    initial commit

---
+++ src/main/java/com/threesome/timeTrackingService/TimeTrackingApplication.java
@@ -0,0 +1,12 @@
+package com.threesome.timeTrackingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTrackingApplication.class, args);
+    }
+}
