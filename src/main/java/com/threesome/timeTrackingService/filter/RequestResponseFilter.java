3856c987d77ac20ed91c33eda807fe848d18634e
PavelGozhii <pavelgozhii@gmail.com>
2020-04-02 22:51:31 +0300

    Update'

--- src/main/java/com/threesome/timeTrackingService/filter/RequestResponseFilter.java
+++ src/main/java/com/threesome/timeTrackingService/filter/RequestResponseFilter.java
@@ -1,4 +1,31 @@
 package com.threesome.timeTrackingService.filter;
 
-public class RequestResponseFilter {
+import org.apache.logging.log4j.core.util.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class RequestResponseFilter extends OncePerRequestFilter implements Filter {
+
+    private static Logger LOG = LoggerFactory.getLogger("RequestResponseFilter");
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        ResponseFilter responseFilter = new ResponseFilter(response);
+        String requestBody = IOUtils.toString(request.getReader());
+        LOG.info(request.getRequestURI() + " " + requestBody);
+        filterChain.doFilter(request, response);
+        responseFilter.setHeader("Access-Control-Allow-Origin", "*");
+    }
+
 }
+
